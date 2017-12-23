0853bdb6061f33f450114108c28e2d7a1623663d
Veer Chandra <veercg@users.noreply.github.com>
2017-12-23 11:49:45 +0530

    added function

--- 2Algorithms/algorithms/durstenfeldShuffle.js
+++ 2Algorithms/algorithms/durstenfeldShuffle.js
@@ -3,4 +3,14 @@
 */
 module.exports = exports = function(input) {
 // complete the function
+  var arr  = input;
+  var newArr = [];
+  while(arr.length>0){
+    var arrLen = arr.length;
+    var randomIndex = Math.floor((Math.random() * arrLen) + 1);
+    newArr.push(arr[randomIndex]);
+    arr.splice(randomIndex,1);
+    
+  }
+  return newArr;
 }
