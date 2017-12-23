1764065842ad5ed1026eae8d78e8b60aa66a0cf7
Veer Chandra <veercg@users.noreply.github.com>
2017-12-23 11:50:25 +0530

    added insertion sort

--- 2Algorithms/algorithms/insertionSort.js
+++ 2Algorithms/algorithms/insertionSort.js
@@ -3,4 +3,18 @@
 */
 module.exports = exports = function(input) {
 // complete the function
+  var arrLen = input.length;
+  var sortedArr = input;
+  for(var i=1; i<arrLen; i++){
+    var j = i-1;
+    while(j>=0 && sortedArr[i]<sortedArr[j]){
+      var temp = sortedArr[j];
+      sortedArr[j] = sortedArr[i];
+      sortedArr[i] = temp;
+      j = j - 1;
+      i = i-1;
+    }
+    
+  }
+  return sortedArr;
 };
